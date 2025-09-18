56b7a016c96565175a279108ed6677cb990c59c2
Andrei Benea <andrei.benea89@gmail.com>
2025-09-18 15:25:35 -0400

    add player logic

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import pygame
 from constants import *
+from player import *
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+updatables = pygame.sprite.Group()
+drawables = pygame.sprite.Group()
+Player.containers = (updatables, drawables)
 
 def main():
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    
     print("Starting Asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
     pygame.init()
+    clock = pygame.time.Clock()
+    player = Player(x, y)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, color="#000000")
+        for drawable in drawables:
+            drawable.draw(screen)
+        updatables.update(dt)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
